0d49dd0824495d1f3dbc93cd742df7fef08b8d64
Pablo Otero <pablooterocastro15@gmail.com>
2026-09-16 13:02:46 +0200

    Update depencencies

--- SmartAC/settings.gradle.kts
+++ SmartAC/settings.gradle.kts
@@ -34,7 +34,7 @@ dependencyResolutionManagement {
     }
 
     versionCatalogs {
-        create("ktorLibs").from("io.ktor:ktor-version-catalog:3.5.1")
+        create("ktorLibs").from("io.ktor:ktor-version-catalog:3.5.2")
     }
 }
 
