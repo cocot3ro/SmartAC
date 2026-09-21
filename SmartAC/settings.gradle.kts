c3453b49aa3ea07a63618cd03f77c1b5293621c6
cocot3ro <pablooterocastro@proton.me>
2026-09-21 20:39:30 +0200

    Update AGP and Ktor version catalog to latest releases

--- SmartAC/settings.gradle.kts
+++ SmartAC/settings.gradle.kts
@@ -34,7 +34,7 @@ dependencyResolutionManagement {
     }
 
     versionCatalogs {
-        create("ktorLibs").from("io.ktor:ktor-version-catalog:3.5.2")
+        create("ktorLibs").from("io.ktor:ktor-version-catalog:3.6.0")
     }
 }
 
