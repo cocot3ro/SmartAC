1528876f73f57c477b4979bfc5aa24f2e04fc788
cocot3ro <pablooterocastro@proton.me>
2026-09-22 17:12:14 +0200

    Added tanstack/react-query-devtools

--- SmartAC/webApp/src/App.tsx
+++ SmartAC/webApp/src/App.tsx
@@ -4,9 +4,11 @@ import EasyModal from "ez-modal-react";
 import {ThemeProvider} from "./context";
 import {lazy} from "react";
 import {ToastContainer} from "react-toastify";
-import {Header, Footer} from "src/components";
+import {Footer, Header} from "src/components";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 import {useTranslation} from "react-i18next";
+
 const Home = lazy(() => import("./pages/Home.tsx"));
 const Users = lazy(() => import("./pages/Users.tsx"));
 const Devices = lazy(() => import("./pages/Devices.tsx"));
@@ -22,34 +24,37 @@ export default function App() {
     return (
         <ThemeProvider>
             <QueryClientProvider client={queryClient}>
-                <EasyModal.Provider>
-                    <MemoryRouter initialEntries={["/"]}>
-                        <div className={"page"}>
-                            <div className={"page-wrapper"}>
-                                <Header/>
-                                <div>
-                                    <Routes>
-                                        <Route path="/" element={<Home/>}/>
-                                        <Route path="/users" element={<Users/>}/>
-                                        <Route path="/devices" element={<Devices/>}/>
-                                    </Routes>
+                <>
+                    <EasyModal.Provider>
+                        <MemoryRouter initialEntries={["/"]}>
+                            <div className={"page"}>
+                                <div className={"page-wrapper"}>
+                                    <Header/>
+                                    <div>
+                                        <Routes>
+                                            <Route path="/" element={<Home/>}/>
+                                            <Route path="/users" element={<Users/>}/>
+                                            <Route path="/devices" element={<Devices/>}/>
+                                        </Routes>
 
-                                    <Footer/>
+                                        <Footer/>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </MemoryRouter>
-                </EasyModal.Provider>
-                <ToastContainer
-                    aria-label={"Notification"}
-                    position="top-right"
-                    autoClose={5000}
-                    hideProgressBar={true}
-                    newestOnTop={true}
-                    closeOnClick={true}
-                    rtl={i18n.dir() === "rtl"}
-                    closeButton={false}
-                />
+                        </MemoryRouter>
+                    </EasyModal.Provider>
+                    <ToastContainer
+                        aria-label={"Notification"}
+                        position="top-right"
+                        autoClose={5000}
+                        hideProgressBar={true}
+                        newestOnTop={true}
+                        closeOnClick={true}
+                        rtl={i18n.dir() === "rtl"}
+                        closeButton={false}
+                    />
+                </>
+                <ReactQueryDevtools buttonPosition="bottom-right" position="right"/>
             </QueryClientProvider>
         </ThemeProvider>
     );
